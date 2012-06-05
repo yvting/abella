19c82d109efd675e094118be2df8b5f34334ac84
Yuting Wang <yuting@cs.umn.edu>
2012-06-05 18:05:50 +0200

    A script to load an example in the toplevel

---
+++ src/example.ml
@@ -0,0 +1,9 @@
+open Abella;;
+open Prover;;
+
+#cd "/home/yuting/Projects/Abella";;
+let exp = "type-uniq.thm";;
+input_files := [exp];;
+set_input ();;
+process ();;
+#cd "/home/yuting/Projects/abella_dev/src"
